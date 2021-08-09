ac4b4a18a78071dfd062e63cfa6bb54a4aa554e3
winniecarole <winniedjoukeng@gmail.com>
2021-08-09 18:54:15 +0200

    add sql abfrage im delete .php

--- php/delete.php
+++ php/delete.php
@@ -8,8 +8,18 @@ if(isset($_GET['id'])){
         $data=htmlspecialchars($data);
         return $data;
     }
-    echo $_GET['id'];
-}else{
+    $id = validateuser($_GET['id']);
+
+    $sql = "DELETE FROM users
+	        WHERE id=$id";
+    $result = mysqli_query($conn, $sql);
+    if ($result) {
+        header("Location: ../read.php?success=successfully deleted");
+    }else {
+        header("Location: ../read.php?error=unknown error occurred&$user_data");
+    }
+
+}else {
     header("Location: ../read.php");
+
 }
-?>
